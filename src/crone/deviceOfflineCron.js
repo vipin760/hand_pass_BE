ea8d15921851cb2618430be02e1c55d82f00296f
vipin760 <vipinm500@gmail.com>
2026-01-20 20:55:25 +0530

    sms service completed

--- src/crone/deviceOfflineCron.js
+++ src/crone/deviceOfflineCron.js
@@ -14,14 +14,13 @@ cron.schedule('* * * * *', async () => {
             AND last_connect_time < NOW() - INTERVAL '10 seconds' RETURNING*
         `);
         // const result = await pool.query(`SELECT * FROM devices WHERE online_status=1`)
-        console.log("<><>result", result.rows)
         const attendanceData = await pool.query(`SELECT work_start_time FROM attendance_settings WHERE is_active = true LIMIT 1`);
         if(attendanceData.rows.length != 0){
             const now  = new Date()
             const currentHMM = String(now.getHours()).padStart(2,'0') +":"+ String(now.getMinutes()).padStart(2,'0')+":"+String(now.getSeconds()).padStart(2,"0");
             const dbHmm = attendanceData.rows[0].work_start_time
                 console.log("<><>dbHmm",dbHmm,"===",currentHMM)
-                await runAttendanceCheck(attendanceData.rows[0].work_start_time)
+                // await runAttendanceCheck(attendanceData.rows[0].work_start_time)
             if(dbHmm === currentHMM){
                await runAttendanceCheck(attendanceData.rows[0].work_start_time)
             }
